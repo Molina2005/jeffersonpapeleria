7189ce73d26640a67c3cf1041e4141c118b4829f
Molina2005 <153105515+Molina2005@users.noreply.github.com>
2024-12-10 11:09:08 -0500

    Add files via upload

---
+++ index.html.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="../JEFFERSONPAPELERIA/css/style.css">
+    <title>JEFFERSON DISTRIBUCIONES Y SERVICIOS</title>
+</head>
+<body>
+<header>
+<input type="checkbox" id="check">
+        <label for="check" class="mostrar-menu">
+            &#8801
+        </label>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <form class="form-inline my-2 my-lg-0">
+    <div class="search-container">
+    <i><img class="logo" src="../JEFFERSONPAPELERIA/img/logo.jpg" alt=""></i>
+    
+
+                <input type="text" placeholder="Busca tu producto" class="search-input">
+                <button class="search-button">Buscar</button>
+                <a href="../JEFFERSONPAPELERIA/login/iniciosesion.php"><img class="logo-sesion" src="../JEFFERSONPAPELERIA/img/inicio.png" alt=""></a>
+            </div>
+    </form>
+  </div>
+</nav>
+</div>
+
+
+<nav class="menu">
+    <ul class="menu-horizontal">
+        <li><a class="inicio" href="../JEFFERSONPAPELERIA/index.php">INICIO</a></li>
+        <li>
+            <a href="../JEFFERSONPAPELERIA/productos/selectcliente.php">PAPELERIA</a>
+            <ul class="menu-vertical">
+                
+            </ul>
+        </li>
+        <li>
+            <a href="../JEFFERSONPAPELERIA/productos/selectcliente.php">ESCOLAR</a>
+            <ul class="menu-vertical">
+               
+            </ul>
+        </li>
+        <li>
+            <a href="../JEFFERSONPAPELERIA/productos/oficina.php">OFICINA</a>
+            <ul class="menu-vertical">
+                
+            </ul>
+        </li>
+        <li>
+            <a href="../JEFFERSONPAPELERIA/productos/arte.php">ARTE</a>
+            <ul class="menu-vertical">
+                
+            </ul>
+        </li>
+        <li><a href="../JEFFERSONPAPELERIA/templates/nosotros.php">NOSOTROS</a></li>
+    </ul>
+    <label for="check" class="esconder-menu">
+        &#215
+    </label>
+    
+</nav> 
+</header>
+
+<section id="banner">
+
+    <div class="contenido-banner">
+
+    <div>
+        <img class="img-fondo" src="../JEFFERSONPAPELERIA/img/fondo.jpg" alt="">
+    </div>
+
+    
+</section>
+
+
+    <div id="informate">
+        <h1 class="titulo-informate">INFORMATE CON NOSOTROS</h1>
+        <div class="slider-box">
+        <ul>
+            <li>
+                <img class="img" src="../JEFFERSONPAPELERIA/img/img1.jpg">
+            </li>
+            <li>
+                <img class="img" src="../JEFFERSONPAPELERIA/img/img2.jpg">
+            </li>
+            <li>
+                <img class="img" src="../JEFFERSONPAPELERIA/img/img3.jpg">
+            </li>
+        </ul>
+    </div>
+
+    </div>
+    
+    <div>
+
+        <h1 class="titulo-marcas">MARCAS ALIADAS</h1><br><br><br><br>
+
+        <div class="slider">
+            <div class="slide-track">
+                <div class="slide">
+                    <img src="../JEFFERSONPAPELERIA/img/norma.png" alt="">
+                </div>
+                <div class="slide">
+                    <img src="../JEFFERSONPAPELERIA/img/primavera.png" alt="">
+                </div>
+                <div class="slide">
+                    <img src="../JEFFERSONPAPELERIA/img/totto.webp" alt="">
+                </div>
+                <div class="slide">
+                    <img src="../JEFFERSONPAPELERIA/img/pelikan.png" alt="">
+                </div>
+                <div class="slide">
+                    <img src="../JEFFERSONPAPELERIA/img/bic.png" alt="">
+                </div>
+                <div class="slide">
+                    <img src="../JEFFERSONPAPELERIA/img/payasito.png" alt="">
+                </div>
+                <div class="slide">
+                    <img src="../JEFFERSONPAPELERIA/img/berol.webp" alt="">
+                </div>
+                <div class="slide">
+                    <img src="../JEFFERSONPAPELERIA/img/norma.png" alt="">
+                </div>
+                <div class="slide">
+                    <img src="../JEFFERSONPAPELERIA/img/primavera.png" alt="">
+                </div>
+                <div class="slide">
+                    <img src="../JEFFERSONPAPELERIA/img/totto.webp" alt="">
+                </div>
+                <div class="slide">
+                    <img src="../JEFFERSONPAPELERIA/img/pelikan.png" alt="">
+                </div>
+                <div class="slide">
+                    <img src="../JEFFERSONPAPELERIA/img/bic.png" alt="">
+                </div>
+                <div class="slide">
+                    <img src="../JEFFERSONPAPELERIA/img/payasito.png" alt="">
+                </div>
+                <div class="slide">
+                    <img src="../JEFFERSONPAPELERIA/img/berol.webp" alt="">
+                </div>
+        </div>
+    </div>
+
+    
+
+<?php include("../JEFFERSONPAPELERIA/templates/footer.php") ?>
+
+</body>
+</html>
